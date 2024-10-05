6702a11de78bc62208c01248006db8a24a7b7b6f
Bi1ku <2008owenshi@gmail.com>
2024-10-05 16:35:00 -0400

    finish basic functionality of bills page

--- src/globals.ts
+++ src/globals.ts
@@ -1,11 +1,13 @@
 export const enhancedFetch = async (
-  type: "GEO" | "CONGRESS",
-  path?: string,
+  type: "GEO" | "CONGRESS" | "CUSTOM",
+  path: string,
 ) => {
   const url =
     type === "CONGRESS"
       ? `https://api.congress.gov/v3/${path}?api_key=${process.env.REACT_APP_CONGRESS_API_KEY}`
-      : `https://api.geocod.io/v1.7/${path}&api_key=${process.env.REACT_APP_GEO_API_KEY}`;
+      : type === "GEO"
+        ? `https://api.geocod.io/v1.7/${path}&api_key=${process.env.REACT_APP_GEO_API_KEY}`
+        : path;
 
   return fetch(url)
     .then((res) => res.json())
--- src/pages/Bills/Bills.tsx
+++ src/pages/Bills/Bills.tsx
@@ -5,34 +5,30 @@ import {
   ArrowLongRightIcon,
 } from "@heroicons/react/20/solid";
 import Title from "./components/Title";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const people = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  // More people...
-];
+const pageSize = 15;
 
 function Bills() {
   const [data, setData] = useState<any>();
   const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const getData = async () => {
       setLoading(true);
-      const res = await enhancedFetch("CONGRESS", "bill");
+      const res = await enhancedFetch(
+        "CUSTOM",
+        `https://api.congress.gov/v3/bill?offset=${(page - 1) * pageSize}&limit=${pageSize}&format=json&api_key=${process.env.REACT_APP_CONGRESS_API_KEY}`,
+      );
 
       setData(res);
       setLoading(false);
-      console.log(res);
     };
 
     getData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="px-44 py-16 relative">
@@ -42,143 +38,134 @@ function Bills() {
         <div className="mt-8 flow-root">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left font-semibold text-gray-900 sm:pl-0"
-                    >
-                      Title
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left font-semibold text-gray-900"
-                    >
-                      Bill #
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left font-semibold text-gray-900"
-                    >
-                      Origin Chamber
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left font-semibold text-gray-900"
-                    >
-                      Congress
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left font-semibold text-gray-900"
-                    >
-                      Updated Date
-                    </th>
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-0"
-                    >
-                      <span className="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200">
-                  {data &&
-                    data.bills.map((bill: any, i: number) => (
-                      <tr key={i}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 max-w-2xl pr-4 truncate">
-                          {bill.title}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {bill.number}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {bill.originChamber}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {bill.congress}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {bill.updateDate}
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                          <button className="text-[#457B9D] hover:text-[#E63946] duration-200">
-                            Details
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-
+              {loading ? (
+                <Skeleton width={1425} height={50.5} count={pageSize + 1} />
+              ) : (
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 pl-4 pr-3 text-left font-semibold text-gray-900 sm:pl-0"
+                      >
+                        Title
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left font-semibold text-gray-900"
+                      >
+                        Bill #
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left font-semibold text-gray-900"
+                      >
+                        Origin Chamber
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left font-semibold text-gray-900"
+                      >
+                        Congress
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left font-semibold text-gray-900"
+                      >
+                        Updated Date
+                      </th>
+                      <th
+                        scope="col"
+                        className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                      >
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {data &&
+                      data.bills.map((bill: any, i: number) => (
+                        <tr key={i}>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 max-w-2xl pr-4 truncate">
+                            {bill.title}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {bill.number}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {bill.originChamber}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {bill.congress}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {bill.updateDate}
+                          </td>
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <button className="text-[#457B9D] hover:text-[#E63946] duration-200">
+                              Details
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                  )
+                </table>
+              )}
               <div className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
                 <div className="-mt-px flex w-0 flex-1">
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  <button
+                    onClick={() => setPage(page - 1)}
+                    className={`inline-flex items-center pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 ${page - 1 < 0 && "cursor-not-allowed"}`}
+                    disabled={page - 1 < 0}
                   >
                     <ArrowLongLeftIcon
                       aria-hidden="true"
                       className="mr-3 h-5 w-5 text-gray-400"
                     />
                     Previous
-                  </a>
+                  </button>
                 </div>
                 <div className="hidden md:-mt-px md:flex">
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  <button
+                    onClick={() => setPage(1)}
+                    className={`inline-flex items-center ${page === 1 ? "text-[#457B9D]" : "text-gray-500"} px-4 pt-4 text-sm font-medium hover:border-gray-300 hover:text-gray-700`}
                   >
                     1
-                  </a>
-                  {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
-                  <a
-                    href="#"
-                    aria-current="page"
-                    className="inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600"
+                  </button>
+                  <button
+                    onClick={() => setPage(2)}
+                    className={`inline-flex items-center   ${page === 2 ? "text-[#457B9D]" : "text-gray-500"} px-4 pt-4 text-sm font-medium hover:border-gray-300 hover:text-gray-700`}
                   >
                     2
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  </button>
+                  <button
+                    onClick={() => setPage(3)}
+                    className={`inline-flex items-center   ${page === 3 ? "text-[#457B9D]" : "text-gray-500"} px-4 pt-4 text-sm font-medium hover:border-gray-300 hover:text-gray-700`}
                   >
                     3
-                  </a>
-                  <span className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500">
+                  </button>{" "}
+                  <span className="inline-flex items-center   px-4 pt-4 text-sm font-medium text-gray-500">
                     ...
                   </span>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                  >
-                    8
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                  >
-                    9
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                  >
-                    10
-                  </a>
+                  <input
+                    type="number"
+                    placeholder="10"
+                    className="w-8 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-4 px-2 text-center"
+                    onBlur={(e) => setPage(+e.target.value)}
+                  />
                 </div>
                 <div className="-mt-px flex w-0 flex-1 justify-end">
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  <button
+                    onClick={() => setPage(page + 1)}
+                    className="inline-flex items-center pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
                     Next
                     <ArrowLongRightIcon
                       aria-hidden="true"
                       className="ml-3 h-5 w-5 text-gray-400"
                     />
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
